7ca57c69458611eb7dfdade0c3bf581117bcfdda
pavankumarb852 <44614368+pavankumarb852@users.noreply.github.com>
2026-01-11 22:19:06 +0530

    Implement multilevel inheritance with classes
    Added multilevel inheritance example with Employee, Programmer, and Manager classes.

--- Chapter 11/04_super.py
+++ Chapter 11/04_super.py
@@ -23,3 +23,54 @@ class Manager(Programmer):
 
 o = Manager()
 print(o.a, o.b, o.c)
+
+
+
+
+############
+
+# Base (Parent) class
+class Employee:
+    def __init__(self):
+        # Constructor of Employee class
+        print("Constructor of Employee")
+
+    # Class variable
+    a = 1
+
+
+# Child class inheriting from Employee
+class Programmer(Employee):
+    def __init__(self):
+        # Constructor of Programmer class
+        print("Constructor of Programmer")
+
+    # Class variable
+    b = 2
+
+
+# Child class inheriting from Programmer (Multilevel Inheritance)
+class Manager(Programmer):
+    def __init__(self):
+        # Calling parent class (Programmer) constructor
+        super().__init__()
+
+        # Constructor of Manager class
+        print("Constructor of Manager")
+
+    # Class variable
+    c = 3
+
+
+# Creating an object of Manager class
+o = Manager()
+
+# Accessing class variables inherited from all parent classes
+print(o.a, o.b, o.c)
+
+
+Output
+Constructor of Programmer
+Constructor of Manager
+1 2 3
+
